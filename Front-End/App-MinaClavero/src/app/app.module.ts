2217a0b65e0bb6c89e7762dc91c0f51c1044d011
Lautaro Martinez <learningprogramingo@gmail.com>
2023-08-12 00:28:48 -0300

    4to commit

--- Front-End/App-MinaClavero/src/app/app.module.ts
+++ Front-End/App-MinaClavero/src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+import { RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { EncuestaComponent } from './components/encuesta/encuesta.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    PageNotFoundComponent,
+    EncuestaComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path: '', component:HomeComponent},
+      {path: 'encuesta', component:EncuestaComponent},
+      {path: 'login', component:LoginComponent},
+      {path: 'dashboard', component:DashboardComponent},
+      {path: '**', component:PageNotFoundComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
